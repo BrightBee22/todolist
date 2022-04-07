ec69e752271c1112eb2a02985e258681bbd38cdc
Afolabi <afolabibright11@gmail.com>
2022-04-07 14:15:02 +0000

    added read functionality

--- application/models.py
+++ application/models.py
@@ -1,8 +1,8 @@
 from application import db
 from datetime import datetime
 
-class Tasks(db.model):
-    id = db.column(db.Integer, primary_key=True)
-    description = db.column(db.String(100), nullable=False)
-    completed = db.column(db.Boolean, nullable=False, default=False)
-    date_created = db.column(db.DateTime, nullable=False, default=datetime=datetime.now())
+class Tasks(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(100), nullable=False)
+    completed = db.Column(db.Boolean, nullable=False, default=False)
+    date_created = db.Column(db.DateTime, nullable=False, default=datetime.now())
