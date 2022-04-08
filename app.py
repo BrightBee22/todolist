fc6e12f29188f7355db79110353928b39299acab
BrightBee22 <101715876+BrightBee22@users.noreply.github.com>
2022-04-08 13:58:56 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,10 @@ from application import app, db
 from os import getenv
 
 if __name__ == "__main__":
-    if getenv ("CREATE_SCHEMA").lower() == "true":
+    
+    if getenv("CREATE_SCHEMA") != None:
+        if getenv ("CREATE_SCHEMA").lower() == "true":
+    
             db.drop_all()
             db.create_all()
 
